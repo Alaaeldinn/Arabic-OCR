621d5b3cfa4ed361a3cc1d0a8fe4ed3a6505c379
The Alaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2024-01-14 16:45:58 +0200

    Create Trocr.py

---
+++ Trocr.py
@@ -0,0 +1,14 @@
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import requests
+from PIL import Image
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
+
+url = "put image link"
+image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+
+pixel_values = processor(image, return_tensors="pt").pixel_values
+generated_ids = model.generate(pixel_values)
+
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
